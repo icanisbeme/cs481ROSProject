0ecfa65cb363e4ce4dc5a6312a39b9a0d505c2fb
Bond Ragsdale <ber0003@mix.wvu.edu>
2019-10-14 12:13:18 -0400

    added navigation for main, added mode toggling

--- rosUI.js
+++ rosUI.js
@@ -11,7 +11,48 @@ function solution(A) {
     
 }
 
-function showSettings(div1,div2){
+function changeDiv(div1,div2, div3){
    document.getElementById(div1).style.display = "none";
-   document.getElementById(div2).style.display = "inline-block";
+   document.getElementById(div2).style.display = "none";
+   document.getElementById(div3).style.display = "inline-block";
 }
+
+
+function modeSwitch() {
+    var x = document.getElementById("modeText");
+    var selectLocation = document.getElementById("selectLocationButton");
+    var deleteLocation = document.getElementById("deleteLocationButton");
+    var setNewDestination = document.getElementById("setNewDestinationButton");
+    if (x.innerHTML === "Manual Mode") {
+      x.innerHTML = "Autonomous Mode";
+      selectLocation.disabled = false;
+      deleteLocation.disabled = false;
+      setNewDestination.disabled = false;
+
+    } else {
+      x.innerHTML = "Manual Mode";
+      selectLocation.disabled = true;
+      deleteLocation.disabled = true;
+      setNewDestination.disabled = true;
+    }
+  }
+
+
+//   function colorChanger(){
+//       var color = document.getElementById("colorDropdown").nodeValue;
+//       var background = document.getElementById("mainDisplayDiv");
+//       if(color === 'Blue'){
+//         background.style.backgroundColor = "blue";
+//       }
+//       if(color === 'Red'){
+//         background.style.backgroundColor = "red";
+//       }
+//       if(color === 'Green'){
+//         background.style.backgroundColor = "green";
+//       }
+//       if(color === 'White'){
+//         background.style.backgroundColor = "white";
+//       }
+
+
+//   }
